cac09ab13d7ba65f5f3bc8929b328869f5863d5b
Eli Saado <eli5saado@gmail.com>
2018-03-09 22:01:53 +0100

    Make all other routes work, we only need content now

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 // Define some components
+const Index = { template: "<h1>aAAAAaAAaAAa</h1>" }
 const AboutUs = { template: "<h1>A short story...</h1>" }
+const Contact = { template: "<h1>BBbbbBBbbBbbbBbb</h1>" }
 
 // Create some routes
 const routes = [
-  { path: '/about', component: AboutUs }
+  { path: '/', component: Index },
+  { path: '/about', component: AboutUs },
+  { path: '/contact', component: Contact}
 ]
 
 // create a router
