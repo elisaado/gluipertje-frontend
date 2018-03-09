c7552a5062123afd9cbf7b837554869755174510
Eli Saado <eli5saado@gmail.com>
2018-03-09 22:42:23 +0100

    Make it fetch stuff

--- js/app.js
+++ js/app.js
@@ -1,19 +1,28 @@
+// Beautiful code
+
 // Define some components
-const Index = { template: "<h1>aAAAAaAAaAAa</h1>" }
-const AboutUs = { template: "<h1>A short story...</h1>" }
-const Contact = { template: "<h1>BBbbbBBbbBbbbBbb</h1>" }
+let Home = "";
+let AboutUs = "";
+let Contact = "";
 
-// Create some routes
-const routes = [
-  { path: '/', component: Index },
-  { path: '/about', component: AboutUs },
-  { path: '/contact', component: Contact}
-]
+// DO STUFF
+const fetchComponents = async () => {
+  Home = {template: await (await fetch("views/index.html")).text()};
+  AboutUs = {template: await (await fetch("views/about.html")).text()};
+  Contact = {template: await (await fetch("views/contact.html")).text()};
+}
 
-// create a router
-const router = new VueRouter({
-  routes: routes
-})
+fetchComponents().then(() => {
+  // Create some routes
+  const routes = [
+    {path: '/', component: Home},
+    {path: '/about', component: AboutUs},
+    {path: '/contact', component: Contact}
+  ]
 
-// Make vue use the route and start the app
-const app = new Vue({router}).$mount('#app')
+  // create a router
+  const router = new VueRouter({routes: routes})
+
+  // Make vue use the route and start the app
+  const app = new Vue({router}).$mount('#app');
+});
